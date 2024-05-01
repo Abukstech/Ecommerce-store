e27b1a12a0742860f1a065b2b4fb85c9781b30b9
Abukstech <abdulmajeedbamidele4@gmail.com>
2024-05-01 17:15:18 +0100

    set up sanity

---
+++ sanity/heroimages.ts
@@ -0,0 +1,17 @@
+export default {
+  name: "heroimage",
+  type: "document",
+  title: "Hero image",
+  fields: [
+    {
+      name: "image1",
+      type: "image",
+      title: "first image",
+    },
+    {
+      name: "image2",
+      type: "image",
+      title: "second image",
+    },
+  ],
+};
---
+++ src/app/lib/sanity.ts
@@ -0,0 +1,15 @@
+import { createClient } from "next-sanity";
+import ImageUrlBuilder from "@sanity/image-url";
+
+export const client = createClient({
+  projectId: "nyxcw174",
+  apiVersion: "2022-03-25",
+  dataset: "production",
+  useCdn: true,
+});
+
+const builder = ImageUrlBuilder(client);
+
+export const urlFor = (source: any) => {
+  return builder.image(source);
+};
