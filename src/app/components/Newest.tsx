704295498194a749fb2a7fcad7fb776607540f7f
Abukstech <abdulmajeedbamidele4@gmail.com>
2025-02-01 17:28:09 +0100

    fix the newest page

--- src/app/components/Newest.tsx
+++ src/app/components/Newest.tsx
@@ -14,7 +14,6 @@ interface Props {
 }
 
 async function getData() {
-  
   const query = `*[_type == 'product'][1...5] | order(_createdAt desc ){
     _id,
     price,
@@ -23,7 +22,7 @@ async function getData() {
     "categoryName": category->name,
     "imageUrl": images[0].asset->url
     
-}`
+}`;
 
   const data = await client.fetch(query);
 
@@ -52,31 +51,29 @@ const Newest = async () => {
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {data.map((product) => (
             <div key={product._id} className="group relative">
-              <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
-                <Image
-                  src={product.imageUrl}
-                  alt=""
-                  className="w-full h-full object-cover object-center lg:h-full lg:w-full"
-                  width={300}
-                  height={300}
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm justify-between">
-                    <Link href={`/product/${product.slug}`}>
-                      {product.name}
-                    </Link>
-                  </h3>
-                  <p className="font-semibold mt-1 text-gray-500">
-                    {product.categoryName}
+              <Link href={`/product/${product.slug}`}>
+                <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
+                  <Image
+                    src={product.imageUrl}
+                    alt=""
+                    className="w-full h-full object-cover object-center lg:h-full lg:w-full"
+                    width={300}
+                    height={300}
+                  />
+                </div>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm justify-between">{product.name}</h3>
+                    <p className="font-semibold mt-1 text-gray-500">
+                      {product.categoryName}
+                    </p>
+                  </div>
+                  <p className="text-sm font-medium text-gray-900">
+                    <span>&#8358;</span>
+                    {product.price}
                   </p>
                 </div>
-                <p className="text-sm font-medium text-gray-900">
-                  <span>&#8358;</span>
-                  {product.price}
-                </p>
-              </div>
+              </Link>
             </div>
           ))}
         </div>
